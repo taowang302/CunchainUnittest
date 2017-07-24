d673d33b326d6c62ec4239389899156d15f9b9bf
tao.wang <taowang@chainresearch.org>
2017-07-24 10:04:58 +0800

    修复代码中的空格不统一问题
    Signed-off-by: tao.wang <taowang@chainresearch.org>

--- script/runcase.py
+++ script/runcase.py
@@ -38,54 +38,54 @@ class  RunCase:
 
             for case_id in range(1, test_case_num+1):
                 db_cursor = db_conn.cursor()
-                 #db2_cursor = db2_conn.cursor()
+                # db2_cursor = db2_conn.cursor()
                 db_cursor.execute(
                     'select t.case_name,t.http_method,t.queryparameters,f.host,r.except_response_code,r.except_response,t.test_method from usercase t,file_bag f,test_result r where f.file_number="{}" and t.case_number={} and r.from_view_id=f.file_number and f.file_number=t.from_view_id'.format(
                         archive_id, case_id))
                 tmp_result = db_cursor.fetchall()[:]
-                 log.debug(tmp_result)
-                 tmp_result=tmp_result[0]
-                 test_data.case_id = case_id
-                 test_data.http_method = tmp_result[1]
-                 test_data.request_name = tmp_result[0]
-                 test_data.request_url = tmp_result[0]
-                 test_data.request_param = tmp_result[2]
-                 test_data.test_method = tmp_result[6]
-                 test_data.except_code = tmp_result[4]
-                 test_data.except_response = tmp_result[5]
-                 test_data.result = ''
-                 test_data.reason = ''
-                 log.info("Test API: {}, Method: {}".format(test_data.request_name,test_data.http_method))
-                 test_suite = unittest.TestSuite()
+                log.debug(tmp_result)
+                tmp_result = tmp_result[0]
+                test_data.case_id = case_id
+                test_data.http_method = tmp_result[1]
+                test_data.request_name = tmp_result[0]
+                test_data.request_url = tmp_result[0]
+                test_data.request_param = tmp_result[2]
+                test_data.test_method = tmp_result[6]
+                test_data.except_code = tmp_result[4]
+                test_data.except_response = tmp_result[5]
+                test_data.result = ''
+                test_data.reason = ''
+                log.info("Test API: {}, Method: {}".format(test_data.request_name, test_data.http_method))
+                test_suite = unittest.TestSuite()
                 test_suite.addTest(
                     TestInterfaceCase(test_data.test_method, test_data, http, db_cursor, log=log, archive_id=archive_id))
-                 runner.run(test_suite)
+                runner.run(test_suite)
                 db_cursor.close()
         elif 0 == run_mode:  
             for case_id in run_case_list:
                 db_cursor = db_conn.cursor()
-                 #db2_cursor = db2_conn.cursor()
+                #db2_cursor = db2_conn.cursor()
                 db_cursor.execute(
                     'select t.case_name,t.http_method,t.queryparameters,f.host,r.except_response_code,r.except_response,t.test_method from usercase t,file_bag f,test_result r where f.file_number="{}" and t.case_number={} and r.from_view_id=f.file_number and f.file_number=t.from_view_id'.format(
                         archive_id, case_id))
                 tmp_result = db_cursor.fetchall()[:]
-                 #log.debug(tmp_result)
-                 tmp_result=tmp_result[0]
-                 test_data.case_id = case_id
-                 test_data.http_method = tmp_result[1]
-                 test_data.request_name = tmp_result[0]
-                 test_data.request_url = tmp_result[0]
-                 test_data.request_param = tmp_result[2]
-                 test_data.test_method = tmp_result[6]
-                 test_data.except_code = tmp_result[4]
-                 test_data.except_response = tmp_result[5]
-                 test_data.result = ''
-                 test_data.reason = ''
-                 log.info("Test API: {}, Method: {}".format(test_data.request_name,test_data.http_method))
-                 test_suite = unittest.TestSuite()
+                # log.debug(tmp_result)
+                tmp_result = tmp_result[0]
+                test_data.case_id = case_id
+                test_data.http_method = tmp_result[1]
+                test_data.request_name = tmp_result[0]
+                test_data.request_url = tmp_result[0]
+                test_data.request_param = tmp_result[2]
+                test_data.test_method = tmp_result[6]
+                test_data.except_code = tmp_result[4]
+                test_data.except_response = tmp_result[5]
+                test_data.result = ''
+                test_data.reason = ''
+                log.info("Test API: {}, Method: {}".format(test_data.request_name, test_data.http_method))
+                test_suite = unittest.TestSuite()
                 test_suite.addTest(
                     TestInterfaceCase(test_data.test_method, test_data, http, db_cursor, log=log, archive_id=archive_id))
-                 runner.run(test_suite)
+                runner.run(test_suite)
                 db_cursor.close()
         else:
             log.error("load a run mode")
