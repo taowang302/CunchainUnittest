a2eb60370edc0afa46c71cd5b350142c3dfc1f17
tao.wang <taowang@chainresearch.org>
2017-07-24 09:43:00 +0800

    代码整合
    Signed-off-by: tao.wang <taowang@chainresearch.org>

--- script/test_log.py
+++
@@ -1,6 +0,0 @@
-import configlog
-
-log = configlog.config_log("DEBUG")
-
-log.debug("this is debug")
-log.info("this is info")
--- script/test_readmysql.py
+++
@@ -1,5 +0,0 @@
-import MySQLdb
-
-conn = MySQLdb.connect(host="localhost",port = 3306,user="root",passwd="jbi123456",db ="webdemo")
-cur = conn.cursor()
-data_nu = cur.execute("select * from unittest")
