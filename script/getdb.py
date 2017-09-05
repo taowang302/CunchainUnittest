e3a29b5a9d15c3790e971b265fe106b934386365
tao.wang <taowang@chainresearch.org>
2017-09-05 11:22:14 +0800

    调整db模块，将sql交由db模块执行，引入dblog可根据配置文件实现是否debug sql
    Signed-off-by: tao.wang <taowang@chainresearch.org>

--- script/getdb.py
+++ script/getdb.py
@@ -18,17 +18,8 @@ class GetDB:
         self.passwd = dbconfig.get('passwd')
         self.db = dbconfig.get('db')
         self.charset = dbconfig.get('charset')
-        # self.dblog = sql_log
-        # self.log.debug(
-        # "db config:\nhost => {}\nport => {}\nuser => {}\npasswd =>{} \ndb => {}\ncharset => {}".format(self.host,
-        #                                                                                                self.port,
-        #                                                                                                self.user,
-        #                                                                                                self.passwd,
-        #                                                                                                self.db,
-        #                                                                                                self.charset))
-        # if sql_log:
-        #     self.conn = self.get_conn()
-        #     self.cursor = self.conn.cursor()
+        self.conn = self.get_conn()
+        self.db_log = sql_log
 
     def get_conn(self):
         try:
@@ -38,10 +29,35 @@ class GetDB:
             self.log.error(e)
             sys.exit()
 
-            # def run_sql(self, sql):
-            #     self.dblog.debug(sql)
-            #     try:
-            #         return self.cursor.execute(sql)
-            #     except:
-            #         self.log.error(sys.exc_info()[1])
-            #         return
+    def run_sql(self, sql):
+        self.db_log.debug(sql)
+        db_cursor = self.conn.cursor()
+        db_cursor.execute(sql)
+        return db_cursor
+
+
+if __name__ == "__main__":
+    import configlog
+
+    dblog = configlog.config_db_log({
+        "log_level": "debug",
+        "debug_db_log": "true",
+        "log_path": "../log/unittest.log",
+        "console_log": 'true'
+    })
+    log = configlog.config_log({
+        "log_level": "debug",
+        "debug_db_log": "true",
+        "log_path": "../log/unittest.log",
+        "console_log": 'true'})
+    db = GetDB({
+        "host": "192.168.168.137",
+        "port": "3306",
+        "user": "root",
+        "passwd": "jbi123456",
+        "db": "unittest",
+        "charset": "utf8"
+    }, log, dblog)
+
+    db_cursor = db.run_sql("SELECT COUNT(file_number)  FROM file_bag where file_number='f_001'")
+    log.info(db_cursor.fetchone()[0])
