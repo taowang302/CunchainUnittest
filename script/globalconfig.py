6fb56c18a06502f4c3463d5d376198dbc137ebe3
tao.wang <taowang@chainresearch.org>
2017-09-11 16:45:21 +0800

    注释掉弃用的方法
    Signed-off-by: tao.wang <taowang@chainresearch.org>

--- script/globalconfig.py
+++ script/globalconfig.py
@@ -58,40 +58,39 @@ class GlobalConfig():
     def clear(self):
         self.db.get_conn().close()
 
-
-
-class Global:
-    def __init__(self):
-        self.log = configlog.config_log('../conf/global_config.ini', 'log_level')
-        # self.dblog = configlog.config_log('../conf/global_config.ini', 'debug_db_log')
-        #self.http = ConfigHttp('../conf/global_config.ini',self.log)
-        self.db = GetDB('../conf/global_config.ini', 'DATABASE', self.log)
-        #self.http = ConfigHttp(self.db1, self.log)
-        self.run_mode_config = ConfigRunMode('../conf/global_config.ini')
-    
-    def get_log(self):
-        return self.log
-
-    def get_http(self, archive_id):
-        return ConfigHttp(self.db.get_conn(), self.log, archive_id)
-
-    def get_output_dir(self):
-        return self.run_mode_config.get_output_dir()
-
-    def get_db_conn(self):
-        return self.db.get_conn()
-
-    def get_run_mode(self):
-        return self.run_mode_config.get_run_mode()
-
-    def get_run_archive_id(self):
-        return self.run_mode_config.get_run_archive_id()
-
-    def get_run_case_list(self):
-        return self.run_mode_config.get_case_list()
-
-    def clear(self):
-        self.db.get_conn().close()
+    #
+    # class Global:
+    #     def __init__(self):
+    #         self.log = configlog.config_log('../conf/global_config.ini', 'log_level')
+    #         # self.dblog = configlog.config_log('../conf/global_config.ini', 'debug_db_log')
+    #         #self.http = ConfigHttp('../conf/global_config.ini',self.log)
+    #         self.db = GetDB('../conf/global_config.ini', 'DATABASE', self.log)
+    #         #self.http = ConfigHttp(self.db1, self.log)
+    #         self.run_mode_config = ConfigRunMode('../conf/global_config.ini')
+    #
+    #     def get_log(self):
+    #         return self.log
+    #
+    #     def get_http(self, archive_id):
+    #         return ConfigHttp(self.db.get_conn(), self.log, archive_id)
+    #
+    #     def get_output_dir(self):
+    #         return self.run_mode_config.get_output_dir()
+    #
+    #     def get_db_conn(self):
+    #         return self.db.get_conn()
+    #
+    #     def get_run_mode(self):
+    #         return self.run_mode_config.get_run_mode()
+    #
+    #     def get_run_archive_id(self):
+    #         return self.run_mode_config.get_run_archive_id()
+    #
+    #     def get_run_case_list(self):
+    #         return self.run_mode_config.get_case_list()
+    #
+    #     def clear(self):
+    #         self.db.get_conn().close()
         #self.db2.get_conn().close()
 
     # def get_dblog(self):
